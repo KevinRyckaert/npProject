9707cde70f78a946ab4edb770cec57035d481f38
KevinRyckaert <kevinryckaert90@gmail.com>
2019-11-26 09:19:45 +0100

    inscription.php

--- inscription.php
+++ inscription.php
@@ -1,6 +1,18 @@
 <?php require("inc/header.php")?>
 
-
+<form action="" method="post">
+    <label for="">Nom</label>
+    <input type="text" name="formNom" id="">
+    <label for="">Prénom</label>
+    <input type="text" name="formPrenom" id="">
+    <label for="">E-mail</label>
+    <input type="email" name="formMail" id="">
+    <label for="">Mot de passe</label>
+    <input type="password" name="formPassword" id="">
+    <label for="">Confirmez votre mot de passe</label>
+    <input type="password" name="formPassword2" id="">
+    <button>S'inscrire</button>
+</form>
 
 
 
