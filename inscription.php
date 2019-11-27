d740cef50e2c543c4f1cdbfd7ab5748a96e376f2
KevinRyckaert <kevinryckaert90@gmail.com>
2019-11-27 16:56:43 +0100

    fin de journee 27/11/2019

--- inscription.php
+++ inscription.php
@@ -2,9 +2,14 @@
 
 
 <?php
+    //require 'recapcha.php';
     if(!empty($_POST))
     {
         $errors=array();
+        var_dump($_POST);
+
+        //$captcha = new Recaptcha('6Lek7cQUAAAAAHwEzQzyNceEhqcYE3tstdoAk3sF');
+        //$captcha -> checkCode($_POST["g-recaptcha-response"]);
 
         if(empty($_POST['pseudo']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['pseudo']))
         {
@@ -22,31 +27,34 @@
         debug($errors);
     }
 
-    if(empty($errors))
+    if(!empty($errors))
     {
         require_once('inc/db.php');
         $req = $pdo -> prepare("INSERT INTO membre SET pseudo = ?, nom = ?, prenom = ?, email = ?, password = ?");
         $password = password_hash($_POST['password'],PASSWORD_BCRYPT);
         $req -> execute($_POST['pseudo'], $_POST['nom'], $_POST['prenom'], $_POST['Mail'], $password);
-            echo "Notre compte a bien été créé";
+            echo "Votre compte a bien été créé";
     }
     
 
 ?>
-<form action="" method="post">
-    <label for="">Pseudo</label>
-    <input type="text" name="pseudo" required>
-    <label for="">Nom</label>
-    <input type="text" name="nom" required>
-    <label for="">Prénom</label>
-    <input type="text" name="prenom" required >
-    <label for="">E-mail</label>
-    <input type="mail" name="mail" required >
-    <label for="">Mot de passe</label>
-    <input type="password" name="password" required>
-    <label for="">Confirmez votre mot de passe</label>
-    <input type="password" name="password2" required>
-    <button>S'inscrire</button>
-</form>
+    <form action="" method="post">
+        <section>
+            <label for="">Pseudo</label>
+            <input type="text" name="pseudo">
+            <label for="">Nom</label>
+            <input type="text" name="nom">
+            <label for="">Prénom</label>
+            <input type="text" name="prenom" >
+            <label for="">E-mail</label>
+            <input type="mail" name="mail" >
+            <label for="">Mot de passe</label>
+            <input type="password" name="password">
+            <label for="">Confirmez votre mot de passe</label>
+            <input type="password" name="password2">
+        </section>
+        <section class="g-recaptcha" data-sitekey="6Lek7cQUAAAAAEtDj_SwR089NnZPVPZta9W9W-QD"></section>
+        <button>S'inscrire</button>
+    </form>
 
 <?php require("inc/footer.php")?>
